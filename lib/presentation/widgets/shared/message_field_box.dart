74a8d1aac3fde3b542c4c4fbf5d30e0610b54b99
Sylvie Salazar <sylvina_srosales@hotmail.com>
2026-07-07 14:30:08 -0600

    feat: chatScreen - MessageFieldBox behavior

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,7 +5,9 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
+    final textController = TextEditingController();
+
+    final focuseNode = FocusNode();
 
     final outlinedInputBorder = UnderlineInputBorder(
       borderSide: BorderSide(color: Colors.transparent),
@@ -13,24 +15,31 @@ class MessageFieldBox extends StatelessWidget {
     );
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with "?"',
       enabledBorder: outlinedInputBorder,
       focusedBorder: outlinedInputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send_outlined),
         onPressed: () {
-          print('Value');
+          final textValue = textController.value.text;
+          print('Value: $textValue');
+          textController.clear();
         },
       ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focuseNode.unfocus();
+      },
+      focusNode: focuseNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Submit value: $value');
-      },
-      onChanged: (value) {
-        print('Changed: $value');
+        textController.clear();
+        focuseNode.requestFocus(); //Recuperamos el focus
       },
     );
   }
