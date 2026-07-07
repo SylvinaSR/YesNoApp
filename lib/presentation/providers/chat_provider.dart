b533de6d95ecceaf16336dcbce4a3681bb45d0b9
Sylvie Salazar <sylvina_srosales@hotmail.com>
2026-07-07 14:50:55 -0600

    feat: functionality - chat provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+/**
+ * ChangeNotifier es una clase nativa de la librería base que sirve para gestionar el estado de tu aplicación. 
+ * Funciona como un sistema de suscripción: 
+ * almacena datos y avisa automáticamente a los widgets (pantallas) que estén escuchando para que se reconstruyan cada vez que esos datos cambian
+ */
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> message = [
+    Message(text: 'Hola amor', fromWho: FromWho.mine),
+    Message(text: 'Regresando del trabajo', fromWho: FromWho.mine)
+  ];
+
+  Future<void> sendMessage(String text) async {
+    //Implementation
+  }
+}
