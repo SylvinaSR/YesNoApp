11f7e75059cca773d055ac012797e37036c0b98d
Sylvie Salazar <sylvina_srosales@hotmail.com>
2026-07-08 16:56:07 -0600

    feat: functionality - show messages from provider

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -8,7 +8,7 @@ import 'package:yes_no_app/domain/entities/message.dart';
  */
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Hola amor', fromWho: FromWho.mine),
     Message(text: 'Regresando del trabajo', fromWho: FromWho.mine)
   ];
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/his_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
@@ -32,6 +35,9 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Esto va a estar pendiente de los cambios, es la instancia de nuestro ChatProvider
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -39,9 +45,12 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
-                itemCount: 10,
+                itemCount: chatProvider.messageList.length,
                 itemBuilder: (context, index) {
-                  return (index % 2 == 0) ? const HisMessageBubble() : const MyMessageBubble();
+                  final message = chatProvider.messageList[index];
+                  return (message.fromWho == FromWho.his)
+                      ? HisMessageBubble()
+                      : MyMessageBubble();
                 },
               ),
             ),
